79bfa701e737b3ddc6e2776976ac75d89aa1e16f
basillgohar <basil.gohar.123@gmail.com>
2026-04-18 17:38:56 +0200

    Add files via upload

--- faceandeye.py
+++ faceandeye.py
@@ -14,8 +14,15 @@ while True:
     faces = face_cascade.detectMultiScale(gray,1.3,5)
     for(x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y + h),(255,0,0),5)
-        roi_gray = gray[y:y+w, x:x+w]
+        roi_gray = gray[y:y+h, x:x+w] #area of initress
 
         roi_color = frame[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray,1.3,5)
-        
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey + eh),(255,0,0),5)   
+
+    cv2.imshow("Frame",frame)
+    if cv2.waitKey(1)==ord('q'):
+        break
+cap.release ()
+cv2.destroyAllWindows()
